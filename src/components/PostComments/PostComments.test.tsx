fbaddd372fde256db66e2a7e2333c09a61740a32
Caroline Urbainski <carolineurb.dev@outlook.com>
2024-11-21 10:03:16 -0300

    test

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,31 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import PostComment from '../PostComments';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    test('Deve adicionar comentário "aaaaaa" no post', () => {
+        render(<PostComment/>);
+        fireEvent.change(screen.getByTestId('comment-area'), {
+            target:{
+                value:'aaaaaa'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comment'))
+        expect(screen.getByText('aaaaaa')).toBeInTheDocument();
+    });
+    test('Deve adicionar comentário "Que legal!" no post', () => {
+        render(<PostComment/>);
+        fireEvent.change(screen.getByTestId('comment-area'), {
+            target:{
+                value:'Que legal!'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comment'))
+        expect(screen.getByText('Que legal!')).toBeInTheDocument();
+    });
+
 });
